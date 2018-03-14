8eb9feeb6259d04b2900cb27ae0e021de886b045
閻　観宇 <sacrificetofree@gmail.com>
2018-03-14 07:31:10 +0000

    solution for reverse-words-in-text

--- double.py
+++ double.py
@@ -1,1 +1,16 @@
-print "America"
+t=['this','is','test']
+a=' '.join(t)
+def reverseWords(s):
+        stuff=s.strip().split()
+        i=len(stuff)-1
+        temp=[]
+        while i>=0:
+            temp.append(stuff[i])
+            i-=1
+        answer=' '.join(temp)
+        return answer
+        #This is the fastest way
+        #return " ".join(s.strip().split()[::-1])
+        
+print reverseWords(a)
+print reverseWords("   a    b    ")
